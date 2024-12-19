71fd2bca4a96bfbc3b2ce1355675da7c383f3257
Jennyk0321 <if112@naver.com>
2024-12-19 22:07:41 +0900

    ha.py

--- ha.py
+++ ha.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('나의 스트림릿 웹앱')
 name = st.text_input('이름을 입력해주세요: ')
-mbti = st.selectboc('mbti를 선택해주세요', ['ENTJ', 'INTP'])
+mbti = st.selectbox('mbti를 선택해주세요', ['ENTJ', 'INTP'])
 
 if st.button('확인!') and name and mbti :
   print(name+ '님은 정말 '+mbti+' 같아보이시네요!')
